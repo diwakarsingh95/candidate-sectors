87f50c141ee1fdceeb802b279127d1e0186dfc77
Diwakar <diwakar.singh313@gmail.com>
2023-12-26 14:06:11 +0530

    added view page

--- src/pages/ViewData/ViewData.tsx
+++ src/pages/ViewData/ViewData.tsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+import SectorForm from "../../components/SectorForm";
+import { MdEditSquare } from "react-icons/md";
+
 const ViewData = () => {
-  return <div>ViewData</div>;
+  const [isEditing, setIsEditing] = useState(false);
+  const onSubmit = async () => {
+    console.log("handleSubmit...");
+  };
+
+  return (
+    <main className="max-w-3xl container px-6 mt-6 sm:mt-10 mx-auto">
+      <div className="flex justify-between items-center mb-6">
+        <h1 className="md:text-center text-2xl font-semibold">Your Data</h1>
+        <button
+          type="button"
+          className="flex items-center gap-1 py-1 px-2 rounded-lg text-sky-600 hover:text-sky-700 focus:outline-sky-500 underline"
+          onClick={() => setIsEditing(!isEditing)}
+        >
+          Edit{" "}
+          <span>
+            <MdEditSquare />
+          </span>
+        </button>
+      </div>
+      <SectorForm onSubmit={onSubmit} />
+    </main>
+  );
 };
 
 export default ViewData;
